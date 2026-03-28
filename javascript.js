e9553633fadf0b9d39d75bf023c548277828b9a1
agfay <6284472+agfay@users.noreply.github.com>
2026-03-28 09:20:15 +0800

    DOM additions of para and heading

--- javascript.js
+++ javascript.js
@@ -5,4 +5,19 @@ const content = document.createElement("div");
 content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
 
+const newPar = document.createElement("p");
+newPar.classList.add("newPar");
+newPar.textContent = "Hi I'm red!";
+newPar.style.color = "Red";
+
+const newHead = document.createElement("h3");
+newHead.classList.add("newHead");
+newHead.textContent = "Hi I'm a blue h3!";
+newHead.style.color = "blue";
+
+const newDiv = document.createElement("div");
+
+
 container.appendChild(content);
+container.appendChild(newPar);
+container.appendChild(newHead);
